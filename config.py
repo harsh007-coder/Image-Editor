9eb9601766779a298508e84b231bdebae3a72e8a
harsh singh <57843859+harsh007-coder@users.noreply.github.com>
2023-12-02 15:16:35 +0530

    Update and rename sample_config.py to config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# By @TroJanzHEX
+
+
+import os
+
+
+class Config(object):
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6467108615:AAGYr0xFmVej6je3TAylviAHKHoECfXD5vM")
+
+    APP_ID = int(os.environ.get("APP_ID", 12345))
+
+    API_HASH = os.environ.get("API_HASH", "97ddeb5f8a91117c3e076b6fb3340b58")
+
+    # Get this api from https://www.remove.bg/b/background-removal-api
+    RemoveBG_API = os.environ.get("RemoveBG_API", "ESSsRj3CpbrRCGsqYFxUEyP1")
